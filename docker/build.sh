d401e58b3c7667f78d7ce8ec31341212edf7c1ec
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2015-07-07 02:54:52 +0300

    Docker fix

--- docker/build.sh
+++ docker/build.sh
@@ -2,20 +2,28 @@
 
 # set -x
 
+echo "Start builder"
+
+PACKAGE_COMPRESS=1
+#PACKAGE_RACE=1
 PACKAGE_GO_IMPORT=`go list -e -f '{{.ImportComment}}' 2>/dev/null || true`
 MAIN_PACKAGE_PATH=$GOPATH"/src/"$PACKAGE_GO_IMPORT
-PACKAGE_COMPRESS="true"
+BUILD_FLAGS=""
 DOCKER_TAG_PREFIX=$1
 
 if [ -n "$DOCKER_TAG_PREFIX" ]; then
     DOCKER_TAG_PREFIX=$DOCKER_TAG_PREFIX"/"
 fi
 
+#if [ $PACKAGE_RACE -eq 1 ]; then
+#    BUILD_FLAGS=$BUILD_FLAGS" -race"
+#fi
+
 # For scratch sub containers
 export GOOS=linux
 export CGO_ENABLED=0
 
-cd $(go env GOROOT)/src && ./make.bash --no-clean && cd -
+# cd $(go env GOROOT)/src && ./make.bash --no-clean && cd -
 
 mkdir -p `dirname $MAIN_PACKAGE_PATH`
 ln -sf /src $MAIN_PACKAGE_PATH
@@ -33,17 +41,19 @@ do
     PACKAGE_NAME=${PACKAGE##*/}
 
     cd $GOPATH"/src/"$PACKAGE
+    go clean
 
     if [ -e "./Makefile" ]; then
+        make build-pre
         make build
     fi
 
-    if [ $? -ne 0 ] || [ ! -e "./Dockerfile" ]; then
-        go build -v -a -tags netgo -ldflags '-w'
+    if [ ! -e "./Makefile" ] || [ $? -ne 0 ]; then
+        go build -v -a -tags netgo -installsuffix netgo -ldflags '-w' $BUILD_FLAGS .
     fi
 
     if [ $? -eq 0 ]; then
-        if [[ $PACKAGE_COMPRESS == "true" ]]; then
+        if [ $PACKAGE_COMPRESS -eq 1 ]; then
           goupx $PACKAGE_NAME
         fi
 
