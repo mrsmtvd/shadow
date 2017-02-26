18f391332fdbe1ded21a759a1b21e408d6d98e66
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2017-02-26 10:35:30 +0300

    Added metrics

--- components/workers/metrics.go
+++ components/workers/metrics.go
@@ -8,6 +8,8 @@ import (
 )
 
 const (
+	MetricListenersTotal  = "workers.listeners.total"
+	MetricListenersTasks  = "workers.listeners.tasks"
 	MetricWorkersTotal  = "workers.workers.total"
 	MetricWorkersStatus = "workers.workers.status"
 	MetricTasksTotal    = "workers.tasks.total"
@@ -15,6 +17,8 @@ const (
 )
 
 var (
+	metricListenersTotal kit.Gauge
+	metricListenersTasks kit.Gauge
 	metricWorkersTotal kit.Gauge
 	metricTasksTotal   kit.Gauge
 
@@ -32,70 +36,48 @@ var (
 )
 
 func (c *Component) MetricsCapture() {
-	workersTotal := 0
-	tasksTotal := 0
-	workersStatusWait := 0
-	workersStatusProcess := 0
-	workersStatusBusy := 0
-	tasksStatusWait := 0
-	tasksStatusProcess := 0
-	tasksStatusSuccess := 0
-	tasksStatusFail := 0
-	tasksStatusFailByTimeout := 0
-	tasksStatusKill := 0
-	tasksStatusRepeatWait := 0
+	metricListenersTotal.Set(float64(len(c.dispatcher.GetListeners())))
+	metricListenersTasks.Set(float64(len(c.dispatcher.GetListenersTasks())))
 
 	for _, w := range c.dispatcher.GetWorkers().GetItems() {
-		workersTotal += 1
+		metricWorkersTotal.Add(1)
 
 		switch w.GetStatus() {
 		case worker.WorkerStatusWait:
-			workersStatusWait += 1
+			metricWorkerStatusWait.Add(1)
 		case worker.WorkerStatusProcess:
-			workersStatusProcess += 1
+			metricWorkerStatusProcess.Add(1)
 		case worker.WorkerStatusBusy:
-			workersStatusBusy += 1
+			metricWorkerStatusBusy.Add(1)
 		}
 	}
 
 	for _, t := range c.dispatcher.GetTasks().GetItems() {
-		tasksTotal += 1
+		metricTasksTotal.Add(1)
 
 		switch t.GetStatus() {
 		case task.TaskStatusWait:
-			tasksStatusWait += 1
+			metricTasksStatusWait.Add(1)
 		case task.TaskStatusProcess:
-			tasksStatusProcess += 1
+			metricTasksStatusProcess.Add(1)
 		case task.TaskStatusSuccess:
-			tasksStatusSuccess += 1
+			metricTasksStatusSuccess.Add(1)
 		case task.TaskStatusFail:
-			tasksStatusFail += 1
+			metricTasksStatusFail.Add(1)
 		case task.TaskStatusFailByTimeout:
-			tasksStatusFailByTimeout += 1
+			metricTasksStatusFailByTimeout.Add(1)
 		case task.TaskStatusKill:
-			tasksStatusKill += 1
+			metricTasksStatusKill.Add(1)
 		case task.TaskStatusRepeatWait:
-			tasksStatusRepeatWait += 1
+			metricTasksStatusRepeatWait.Add(1)
 		}
 	}
-
-	metricWorkersTotal.Set(float64(workersTotal))
-	metricTasksTotal.Set(float64(tasksTotal))
-
-	metricWorkerStatusWait.Set(float64(workersStatusWait))
-	metricWorkerStatusProcess.Set(float64(workersStatusProcess))
-	metricWorkerStatusBusy.Set(float64(workersStatusBusy))
-
-	metricTasksStatusWait.Set(float64(tasksStatusWait))
-	metricTasksStatusProcess.Set(float64(tasksStatusProcess))
-	metricTasksStatusSuccess.Set(float64(tasksStatusSuccess))
-	metricTasksStatusFail.Set(float64(tasksStatusFail))
-	metricTasksStatusFailByTimeout.Set(float64(tasksStatusFailByTimeout))
-	metricTasksStatusKill.Set(float64(tasksStatusKill))
-	metricTasksStatusRepeatWait.Set(float64(tasksStatusRepeatWait))
 }
 
 func (c *Component) MetricsRegister(m *metrics.Component) {
+	metricListenersTotal = m.NewGauge(MetricListenersTotal)
+	metricListenersTasks = m.NewGauge(MetricListenersTasks)
+
 	metricWorkersTotal = m.NewGauge(MetricWorkersTotal)
 	metricTasksTotal = m.NewGauge(MetricTasksTotal)
 
