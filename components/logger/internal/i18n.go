6006ca71edad078c9727654eb9c6b78915d701e6
kihamo <dev@kihamo.ru>
2018-03-25 18:37:04 +0300

    i18n for logger

---
+++ components/logger/internal/i18n.go
@@ -0,0 +1,17 @@
+package internal
+
+import (
+	"io"
+
+	"github.com/elazarl/go-bindata-assetfs"
+	"github.com/kihamo/shadow/components/i18n"
+)
+
+func (c *Component) I18n() map[string][]io.ReadSeeker {
+	return i18n.FromAssetFS(&assetfs.AssetFS{
+		Asset:     Asset,
+		AssetDir:  AssetDir,
+		AssetInfo: AssetInfo,
+		Prefix:    "locales",
+	})
+}
