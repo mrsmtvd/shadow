8d2ab8b2104a75eff65e29176eeac51032a58b48
kihamo <dev@kihamo.ru>
2018-10-18 14:23:42 +0300

    Start span helper

--- components/tracing/span.go
+++ components/tracing/span.go
@@ -1,6 +1,7 @@
 package tracing
 
 import (
+	"context"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/opentracing/opentracing-go/log"
@@ -9,4 +10,11 @@ import (
 func SpanError(span opentracing.Span, err error) {
 	span.LogFields(log.Error(err))
 	ext.Error.Set(span, true)
+}
+
+func StartSpanFromContext(ctx context.Context, componentName, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
+	tag := opentracing.Tag{Key: string(ext.Component), Value: componentName}
+	opts = append([]opentracing.StartSpanOption{tag}, opts...)
+
+	return opentracing.StartSpanFromContext(ctx, operationName, opts...)
 }
