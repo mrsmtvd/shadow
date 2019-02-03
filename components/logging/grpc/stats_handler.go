3b5d49b34a4cbf0699048caf9ce5b269a889bf29
kihamo <dev@kihamo.ru>
2019-02-03 13:20:45 +0300

    Fix GRPC log

--- components/logging/grpc/stats_handler.go
+++ components/logging/grpc/stats_handler.go
@@ -24,63 +24,63 @@ func NewStatsHandler(log logging.Logger) *StatsHandler {
 
 func (h *StatsHandler) HandleConn(ctx context.Context, stat stats.ConnStats) {
 	ctxValue := h.ConnectValueFromContext(ctx)
-	fields := map[string]interface{}{
-		"remote-address": ctxValue.RemoteAddr,
-		"local-address":  ctxValue.LocalAddr,
+	fields := []interface{}{
+		"remote-address", ctxValue.RemoteAddr.String(),
+		"local-address", ctxValue.LocalAddr.String(),
 	}
 
 	switch stat.(type) {
 	case *stats.ConnBegin:
-		h.log.Info("GRPC connect is open", fields)
+		h.log.Info("GRPC connect is open", fields...)
 
 	case *stats.ConnEnd:
-		h.log.Info("GRPC connect is closed", fields)
+		h.log.Info("GRPC connect is closed", fields...)
 	}
 }
 
 func (h *StatsHandler) HandleRPC(ctx context.Context, stat stats.RPCStats) {
 	ctxValue := h.RPCValueFromContext(ctx)
-	fields := map[string]interface{}{
-		"service": ctxValue.Service,
-		"method":  ctxValue.Method,
-		"type":    ctxValue.Type,
-		"client":  ctxValue.ClientName,
+	fields := []interface{}{
+		"service", ctxValue.Service,
+		"method", ctxValue.Method,
+		"type", ctxValue.Type,
+		"client", ctxValue.ClientName,
 	}
 
 	switch s := stat.(type) {
 	case *stats.InPayload:
 		if s.IsClient() {
-			fields["response"] = s.Payload
+			fields = append(fields, "response", s.Payload)
 		} else {
-			fields["request"] = s.Payload
+			fields = append(fields, "request", s.Payload)
 		}
 
-		h.log.Info("Call gRPC method", fields)
+		h.log.Info("Call gRPC method", fields...)
 
 	case *stats.OutPayload:
 		if s.IsClient() {
-			fields["request"] = s.Payload
+			fields = append(fields, "request", s.Payload)
 		} else {
-			fields["response"] = s.Payload
+			fields = append(fields, "response", s.Payload)
 		}
 
-		h.log.Info("Call gRPC method", fields)
+		h.log.Info("Call gRPC method", fields...)
 
 	case *stats.End:
 		code := grpc_logging.DefaultErrorToCode(s.Error)
-		fields["code"] = code
+		fields = append(fields, "code", code)
 
 		if s.Error != nil {
-			fields["error"] = s.Error.Error()
+			fields = append(fields, "error", s.Error.Error())
 		}
 
 		switch code {
 		case codes.OK:
-			h.log.Info("Called gRPC method", fields)
+			h.log.Info("Called gRPC method", fields...)
 		case codes.OutOfRange, codes.Internal, codes.Unavailable, codes.DataLoss:
-			h.log.Error("Called gRPC method", fields)
+			h.log.Error("Called gRPC method", fields...)
 		default:
-			h.log.Info("Called gRPC method", fields)
+			h.log.Info("Called gRPC method", fields...)
 		}
 	}
 }
